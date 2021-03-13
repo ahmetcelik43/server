2eacf301dbbed72f4073b67eb27b8f61186cb101
ahmetcelik43 <45917904+ahmetcelik43@users.noreply.github.com>
2021-03-14 00:05:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 //AIzaSyAm5-b8unXmYcXelxm8j7X2csln6trDwME
 const webPush = require("web-push");
-const vapidKeys = webPush.generateVAPIDKeys();
-console.log(vapidKeys);/*
+//const vapidKeys = webPush.generateVAPIDKeys();
+//console.log(vapidKeys);
 const vapidKeys =
 {
-  publicKey: 'BIBMFSOF20Osx84Suo3oI5a1XBiqd1rsc7B4923bJWVZaJ7Qsdewy1pGqZX9uu_UXtRzXxoR6g105PExKYPvUKQ',
-  privateKey: 'cXyLDw4gS6aPcsTcFY7rTdiElW4sZqAd4D9tBd7VIwU'
+  publicKey: 'BO_GWC2iDnuJb2oGUSwC9SiUcvpM-HcklMDXDR-UV4WJ8ziq23u1JDntFrCu-h8W8XglgzJV4xutwPefTqBbjGA',
+  privateKey: 'l9jc3ut4N-4o25N4EKKENgwiUC1YBcbh27s8dCzrvCo'
 }
 webPush.setGCMAPIKey('AIzaSyAm5-b8unXmYcXelxm8j7X2csln6trDwME');
 
@@ -42,6 +42,6 @@ app.post("/send_notification", (req, res) => {
   app.listen(PORT, () => {
     console.log("Sunucu Başarılı bir şekilde çalışıyor...");
   });
-  */
+  
   /*
   {"endpoint":"https://fcm.googleapis.com/fcm/send/czmAx9qMoQE:APA91bGfMbCuWKFWDzMQ8skn76jxJptSrmN3w7M8gF7wR5NGX2ZBzZVR_buyXnWGCKFGEKJ_4Vp-YkDg6TJWG2Ir8U0MeNls997-PPMEb8GnxpZcoXmTXgY91YYDO2zNteGpI3gs5iwO","expirationTime":null,"keys":{"p256dh":"BJ6CnGvYd0p4tOcNCR22ytEaWkLaqZXFmHo9bycAwVb_iCkKA5-dYUxDpM1-IUMhJrgdfExtVgz6zEK1BgvCqqg","auth":"Alu9hVnh1dg6ePXdsd-8Uw"}}*/ 
